1b4726d3bb120fb172fd29b81e4ff67f9927f5db
Stephen Smith <blackrim@gmail.com>
2017-04-22 13:49:20 -0400

    cleaning

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import sys,os
 import tree_reader,node
 import math
+from utils import *
 import numpy as np
 from numpy import *
 from anc_state_calculator import *
@@ -10,46 +11,14 @@ from scipy.stats import gaussian_kde as kde
 from scipy import stats
 import matplotlib.pyplot as plt
 
+#this is the number of cuts
 ncuts = 100
+#this is the range of the data
 low = 0
 high = 6
 cuts = []
 for i in range(ncuts+1):
     cuts.append(low + (i*(float(high-low)/ncuts)))
-
-"""
-matches to cont values
-"""
-def match_tips_and_cont_values(tree,seqs):
-    lvs = tree.leaves()
-    for i in lvs:
-        test = False
-        for j in seqs:
-            if j.name == i.label:
-                i.data['cont_values'] = j.cont_values
-                test = True
-                break
-        if test == False:
-            print "can't find "+i.label+" in cont_values"
-            return False
-
-
-def bimodal( low1, high1, mode1, low2, high2, mode2 ,N):
-    ns = []
-    for i in range(N):
-        toss = np.random.choice( (1, 2) )
-        if toss == 1:
-            ns.append( np.random.triangular( low1, mode1, high1 ) )
-        else:
-            ns.append( np.random.triangular( low2, mode2, high2 ))
-    return ns
-
-def aic(k,l):
-    return (2*k)+(2*l)
-
-def aicc(k,l,n):
-    return aic(k,l) + ((2*k*(k+1))/(n-k-1))
-
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
